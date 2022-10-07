2a4ec1ee7d95b227ba9dd8e1751b766f97d1f546
Igor Gabriel <97804247+igorUfca@users.noreply.github.com>
2022-10-07 17:03:04 -0300

    Virus 1.0 Estrutura de repetição

---
+++ Virus.py
@@ -0,0 +1,11 @@
+print ("Crie seu virus ")
+print ("___________________________")
+print ()
+x = input("Digite o nome do arquivo virus que deseja criar: ")
+
+def main():
+    q=0
+    while 1:
+        q = q + 1
+        print ("Virus ", x, "copias: ", q )
+main()        
